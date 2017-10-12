c2c94f60cb562d755209074db88ede2298a05e44
Adheen Ajay <adheen@gmail.com>
2017-10-12 22:28:29 +0900

    Add code file

---
+++ rnn.py
@@ -0,0 +1,3 @@
+# ref: http://nbviewer.jupyter.org/github/donnemartin/data-science-ipython-notebooks/blob/master/deep-learning/tensor-flow-examples/notebooks/3_neural_networks/recurrent_network.ipynb
+
+# import input_data
